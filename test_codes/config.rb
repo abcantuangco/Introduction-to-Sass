6f4a0bf117bcc9b773878bcc535518f25427e5b0
Arsenio Cantuangco Jr <abcantuangco@gmail.com>
2016-06-22 21:32:09 +0800

    update test codes

--- test_codes/config.rb
+++ test_codes/config.rb
@@ -1,4 +1,4 @@
-require 'compass/import-once/activate'
+# require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
@@ -7,6 +7,7 @@ css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
+http_generated_images_path = "/test_codes/images"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
